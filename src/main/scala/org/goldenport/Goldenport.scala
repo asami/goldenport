9b7c5feb53357373d551865bcf5160d542f318b2
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2012-11-16 08:03:09 +0900

    Version 0.4.5 (2012-11-16)

--- src/main/scala/org/goldenport/Goldenport.scala
+++ src/main/scala/org/goldenport/Goldenport.scala
@@ -27,12 +27,12 @@ import org.goldenport.entity.datasource.StringDataSource
  * @since   Aug. 28, 2008
  *  version Feb. 21, 2012
  *  version Sep. 25, 2012
- * @version Nov. 14, 2012
+ * @version Nov. 16, 2012
  * @author  ASAMI, Tomoharu
  */
 class Goldenport(theArgs: Array[String], aDesc: GApplicationDescriptor) extends GoldenportConstants {
   val version = "0.4.5"
-  val build = "20121114"
+  val build = "20121116"
   private var _system_parameters = setup_system_parameters
   private var _container_parameters = setup_container_parameters
   private var _application_parameters = setup_application_parameters(aDesc)
